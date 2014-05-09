c316b3005e3a584445623176d7a6a5a0ad6bf697
Adrian Martinez <am@bloombees.com>
2014-05-09 06:48:30 +0200

    Updates

--- ADNBP/logic/CloudFrameWorkService.php
+++ ADNBP/logic/CloudFrameWorkService.php
@@ -2,27 +2,27 @@
 
 // This service has to be implemented in your <document_root>/logic/CloudFrameWorkService.php
 
-if(is_file("./logic/CloudFrameWorkService.php")) {    
-    include("./logic/CloudFrameWorkService.php");  
-} {
     list($foo,$script,$service,$params) = split('/',$this->_url,4);
+    
     switch ($service) {
-    	case 'getTemplate':
+        case 'getTemplate':
             $template =$params;
             if(is_file("./ADNBP/templates/CloudFrameWork/".$template)) {
                echo(file_get_contents ( "./ADNBP/templates/CloudFrameWork/".$template ));
             }
-            else echo("template $template not found");     
-    		break;
+            else echo("template $template not found");   
+			die();  
+            break;
         case 'checkVersion':
             echo(($this->version == $params)?"OK $params":"ERROR. Your version  ".htmlentities($params)." is different of current version:".$this->version);
+            die();
+		    break;
+        default:
+			// This allow to create own services in each WebServer
+            if(is_file("./logic/CloudFrameWorkService/".$service.".php"))
+                include_once "./logic/CloudFrameWorkService/".$service.".php";
+            else echo "The Service <b>".htmlentities($service)."</b> is not installed";
             break;
-    	default:
-    		echo "Wrong paramaters";
-    		break;
     }
-}
-
-die();
 
 ?>
