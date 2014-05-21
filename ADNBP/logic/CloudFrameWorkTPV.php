e9e3f189f758cb58d0a7482c7eba20922a10d8fe
Adrián Martínez <am@bloombees.com>
2014-05-21 08:31:40 +0200

    Improvements

--- ADNBP/logic/CloudFrameWorkTPV.php
+++ ADNBP/logic/CloudFrameWorkTPV.php
@@ -15,9 +15,13 @@
                 if(strlen($this->getConf("Ds_Merchant_MerchantCode"))) $_data[Ds_Merchant_MerchantCode] = $this->getConf("Ds_Merchant_MerchantCode");
                 if(strlen($this->getConf("Ds_Merchant_Terminal"))) $_data[Ds_Merchant_Terminal] = $this->getConf("Ds_Merchant_Terminal");
                 if(strlen($this->getConf("Ds_Merchant_MerchantURL"))) $_data[Ds_Merchant_MerchantURL] = $this->getConf("Ds_Merchant_MerchantURL");
+                if(strlen($this->getConf("Ds_Merchant_UrlOK"))) $_data[Ds_Merchant_UrlOK] = $this->getConf("Ds_Merchant_UrlOK");
+                if(strlen($this->getConf("Ds_Merchant_UrlKO"))) $_data[Ds_Merchant_UrlKO] = $this->getConf("Ds_Merchant_UrlKO");
                 if(strlen($this->getConf("Ds_Merchant_Amount"))) $_data[Ds_Merchant_Amount] = $this->getConf("Ds_Merchant_Amount");
                 if(strlen($this->getConf("Ds_Merchant_Order"))) $_data[Ds_Merchant_Order] = $this->getConf("Ds_Merchant_Order");
-                
+                if(strlen($this->getConf("Ds_Merchant_MerchantData"))) $_data[Ds_Merchant_MerchantData] = $this->getConf("Ds_Merchant_MerchantData");
+                if(strlen($this->getConf("Ds_Merchant_MerchantName"))) $_data[Ds_Merchant_MerchantName] = $this->getConf("Ds_Merchant_MerchantName");
+                                                
                 // Allowing _GET params
                 if(!strlen($_data[Ds_Merchant_Amount])) $_data[Ds_Merchant_Amount] = $_GET[Ds_Merchant_Amount];
                 if(!strlen($_data[Ds_Merchant_Order])) $_data[Ds_Merchant_Order] = $_GET[Ds_Merchant_Order];
@@ -26,15 +30,25 @@
                 $_ready = true;
                 if(!strlen($_data[TPV_URL])) $_ready = false;
                 if(!strlen($_data[TPV_Secret])) $_ready = false;
+                if(!strlen($_data[Ds_Merchant_MerchantCode])) $_ready = false;
+                if(!strlen($_data[Ds_Merchant_MerchantName])) $_ready = false;
+                if(!strlen($_data[Ds_Merchant_Titular])) $_ready = false;
+                if(!strlen($_data[Ds_Merchant_ProductDescription])) $_ready = false;
                 if(!strlen($_data[Ds_Merchant_Amount])) $_ready = false;
                 if(!strlen($_data[Ds_Merchant_TransactionType])) $_ready = false;
                 if(!strlen($_data[Ds_Merchant_Currency])) $_ready = false;
-                if(!strlen($_data[Ds_Merchant_MerchantCode])) $_ready = false;
                 if(!strlen($_data[Ds_Merchant_Terminal])) $_ready = false;
                 if(!strlen($_data[Ds_Merchant_MerchantURL])) $_ready = false;
+                if(!strlen($_data[Ds_Merchant_UrlOK])) $_ready = false;
+                if(!strlen($_data[Ds_Merchant_UrlKO])) $_ready = false;
                 if(!strlen($_data[Ds_Merchant_Amount])) $_ready = false;
                 if(!strlen($_data[Ds_Merchant_Order])) $_ready = false;
 
+                //To add information in the URLOK AND KO
+                if($_ready) {
+                    $_data[Ds_Merchant_UrlOK] .= '?Ds_Order='.urlencode($_data[Ds_Merchant_Order]);
+                    $_data[Ds_Merchant_UrlKO] .= '?Ds_Order='.urlencode($_data[Ds_Merchant_Order]);
+                }
                 
             $Ds_Merchant_MerchantSignature = strtoupper(sha1(
                 $_data[Ds_Merchant_Amount]
@@ -45,48 +59,74 @@
                .$_data[Ds_Merchant_MerchantURL]
                .$_data[TPV_Secret]
                ));
-            } else if($params=='response') {
-                $msgerror='';
-                if(!strlen($_POST[Ds_Date])) $msgerror.="-Missing Ds_Date\n";
-                if(!strlen($_POST[Ds_Hour])) $msgerror.="-Missing Ds_Hour\n";
-                if(!strlen($_POST[Ds_Amount])) $msgerror.="-Missing Ds_Amount\n";
-                if(!strlen($_POST[Ds_Currency])) $msgerror.="-Missing Ds_Currency\n";
-                if(!strlen($_POST[Ds_Order])) $msgerror.="-Missing Ds_Order\n";
-                if(!strlen($_POST[Ds_MerchantCode])) $msgerror.="-Missing Ds_MerchantCode\n";
-                if(!strlen($_POST[Ds_Terminal])) $msgerror.="-Missing Ds_Terminal\n";
-                if(!strlen($_POST[Ds_Signature])) $msgerror.="-Missing Ds_Signature\n";
-                if(!strlen($_POST[Ds_Response])) $msgerror.="-Missing Ds_Response\n";
-                if(!strlen($_POST[Ds_TransactionType])) $msgerror.="-Missing Ds_TransactionType\n";
-                if(!strlen($_POST[Ds_SecurePayment])) $msgerror.="-Missing Ds_SecurePayment\n";
-                if(!strlen($_POST[Ds_MerchantData])) $msgerror.="-Missing Ds_MerchantData\n";
-                if(!strlen($_POST[Ds_Card_Country])) $msgerror.="-Missing Ds_Card_Country\n";
-                if(!strlen($_POST[Ds_AuthorisationCode])) $msgerror.="-Missing Ds_AuthorisationCode\n";
-                if(!strlen($_POST[Ds_ConsumerLanguage])) $msgerror.="-Missing Ds_ConsumerLanguage\n";
-                if(!strlen($_POST[Ds_Card_Type])) $msgerror.="-Missing Ds_Card_Type\n";
+            } else if($params=='response' || $params=='OK' || $params=='KO') {
                 
                 if(!is_object($db)){
                     $this->loadClass("db/CloudSQL");
                     $db = new CloudSQL();
-                }  
-                if($db->connect()) {
-                        // Insert Log
-                        $_CloudFrameWorkData[TPVLog_DirectoryOrganization_Id] = $this->getConf("setOrganizationId");
-                        $_CloudFrameWorkData[TPVLog_TPV_Id] = 1; // Sabadell will have 1
-                        
-                        $_CloudFrameWorkData[TPVLog_Name] = ((strlen($msgerror))?'Error ':'OK ').'Log Sabadell TPV response'; // Sabadell will have 1
-                        $_CloudFrameWorkData[TPVLog_Date] = date("Y-m-d H:i:s"); // Sabadell will have 1
-                        $_CloudFrameWorkData[TPVLog_Info] = $msgerror." -> ".print_r($_POST,true); // Sabadell will have 1
-                        if(!$db->cloudFrameWork("insert",$_CloudFrameWorkData,'TPVLogs')) 
-                          $msgerror.= 'DBINSERT_ERROR: '.$db->getError();
-                                                                                            
-                  $db->close(); 
-                } else {
-                  $msgerror.= '- Error connection DB: '.$db->getError();
                 }
+                if(!$db->connect()) die('- Error connection DB: '.$db->getError());
                 
-                if(!strlen($msgerror)) echo "OK";
-                else echo "<pre>$msgerror<pre>";
-                
+                if($params=='OK' || $params=='KO') {
+                   
+                   if(!strlen($_GET[Ds_Order])) $msgerror.="-Missing Ds_Order\n"; 
+                   
+                   
+                   // What to show in the screen
+                   if($params=='OK') $output = 'Transaction OK';
+                   else if($params=='KO') $output = 'Transaction FAILED';
+                   echo $output;
+                   
+                      // Insert Log
+                    $_CloudFrameWorkData[TPVLog_DirectoryOrganization_Id] = $this->getConf("setOrganizationId");
+                    $_CloudFrameWorkData[TPVLog_TPV_Id] = 1; // Sabadell will have 1
+                    $_CloudFrameWorkData[TPVLog_Order] = $_GET[Ds_Order]; // Sabadell will have 1
+                    $_CloudFrameWorkData[TPVLog_Name] = ((strlen($msgerror) || $params=='KO')?'Error ':'OK ').'Log Sabadell TPV '.$params; // Sabadell will have 1
+                    $_CloudFrameWorkData[TPVLog_Date] = date("Y-m-d H:i:s"); // Sabadell will have 1
+                    $_CloudFrameWorkData[TPVLog_Info] = $msgerror." -> ".print_r($_GET,true); // Sabadell will have 1
+                    if(!$db->cloudFrameWork("insert",$_CloudFrameWorkData,'TPVLogs')) 
+                      $msgerror.= 'DBINSERT_ERROR: '.$db->getError();
+                                                                                                
+                    if(strlen($msgerror)) echo "<pre>$msgerror<pre>";                 
+                   
+                    
+                } if($params=='response') {
+                    $msgerror='';
+                    if(!strlen($_POST[Ds_Date])) $msgerror.="-Missing Ds_Date\n";
+                    if(!strlen($_POST[Ds_Hour])) $msgerror.="-Missing Ds_Hour\n";
+                    if(!strlen($_POST[Ds_Amount])) $msgerror.="-Missing Ds_Amount\n";
+                    if(!strlen($_POST[Ds_Currency])) $msgerror.="-Missing Ds_Currency\n";
+                    if(!strlen($_POST[Ds_Order])) $msgerror.="-Missing Ds_Order\n";
+                    if(!strlen($_POST[Ds_MerchantCode])) $msgerror.="-Missing Ds_MerchantCode\n";
+                    if(!strlen($_POST[Ds_Terminal])) $msgerror.="-Missing Ds_Terminal\n";
+                    if(!strlen($_POST[Ds_Signature])) $msgerror.="-Missing Ds_Signature\n";
+                    if(!strlen($_POST[Ds_Response])) $msgerror.="-Missing Ds_Response\n";
+                    if(!strlen($_POST[Ds_TransactionType])) $msgerror.="-Missing Ds_TransactionType\n";
+                    if(!strlen($_POST[Ds_SecurePayment])) $msgerror.="-Missing Ds_SecurePayment\n";
+                    // Optional
+                    // if(!strlen($_POST[Ds_MerchantData])) $msgerror.="-Missing Ds_MerchantData\n";
+                    if(!strlen($_POST[Ds_Card_Country])) $msgerror.="-Missing Ds_Card_Country\n";
+                    if(!strlen($_POST[Ds_AuthorisationCode])) $msgerror.="-Missing Ds_AuthorisationCode\n";
+                    if(!strlen($_POST[Ds_ConsumerLanguage])) $msgerror.="-Missing Ds_ConsumerLanguage\n";
+                    //Optional
+                    //if(!strlen($_POST[Ds_Card_Type])) $msgerror.="-Missing Ds_Card_Type\n";
+                    
+                    // Insert Log
+                    $_CloudFrameWorkData[TPVLog_DirectoryOrganization_Id] = $this->getConf("setOrganizationId");
+                    $_CloudFrameWorkData[TPVLog_TPV_Id] = 1; // Sabadell will have 1
+                    $_CloudFrameWorkData[TPVLog_Order] = $_POST[Ds_Order]; // Sabadell will have 1
+                                        
+                    $_CloudFrameWorkData[TPVLog_Name] = ((strlen($msgerror))?'Error ':'OK ').'Log Sabadell TPV response'; // Sabadell will have 1
+                    $_CloudFrameWorkData[TPVLog_Date] = date("Y-m-d H:i:s"); // Sabadell will have 1
+                    $_CloudFrameWorkData[TPVLog_Info] = $msgerror." -> ".print_r($_POST,true); // Sabadell will have 1
+                    if(!$db->cloudFrameWork("insert",$_CloudFrameWorkData,'TPVLogs')) 
+                      $msgerror.= 'DBINSERT_ERROR: '.$db->getError();
+                                                                                                
+                    if(!strlen($msgerror)) echo "OK";
+                    else echo "<pre>$msgerror<pre>";
+                }
+
+                $db->close();
                 exit;
                                 
                                 
--- ADNBP/templates/CloudFrameWorkTPV.php
+++ ADNBP/templates/CloudFrameWorkTPV.php
@@ -22,8 +22,17 @@
         Production:
         <ul>
          <li><a href='https://sis.redsys.es/canales/' target='_blank'>https://sis.redsys.es/canales/</a>
-             
-         </ul>    
+         </ul> 
+         
+     <b>SOPORTE TECNICO<br/>
+        De lunes a domingo de 8 h a 22 h<br/>
+        Teléfono: 902 365 650 (opc. 2)<br/>
+        Correo electrónico: <br/>
+        tpvvirtual@bancsabadell.com<br/>
+        <br/> 
+        incidencias sobre comunicaciones, inestabilidad del  sistema y similares <br/>
+        teléfono 902 198 747, en activo las 24 horas
+     </b>   
                 
     
   <?php if($_ready) {?>
@@ -31,10 +40,27 @@
      <form action='<?=(!strlen($_data[TPV_URL]))?'':htmlentities($_data[TPV_URL])?>' method ='post' target='tpv'>
     <table>
     <tr><td colspan=2>Send order to: <?=htmlentities($_data[type])?></td></tr>
-    <tr><td>Ds_Merchant_Amount</td>
+    <tr><td>Ds_Merchant_MerchantCode: </td>
+        <td>
+    <input type=hidden name=Ds_Merchant_MerchantCode value='<?=htmlentities($_data[Ds_Merchant_MerchantCode])?>'> <?=htmlentities($_data[Ds_Merchant_MerchantCode])?>
+            
+    </td></tr>
+    <tr><td>Ds_Merchant_MerchantName: </td>
+        <td>
+    <input type=hidden name=Ds_Merchant_MerchantName value='<?=htmlentities($_data[Ds_Merchant_MerchantName])?>'> <?=htmlentities($_data[Ds_Merchant_MerchantName])?>
+    </td></tr>    
+    <tr><td>Ds_Merchant_Titular: </td>
+        <td>
+    <input type=hidden name=Ds_Merchant_Titular value='<?=htmlentities($_data[Ds_Merchant_Titular])?>'> <?=htmlentities($_data[Ds_Merchant_Titular])?>
+    </td></tr>
+    <tr><td>Ds_Merchant_ProductDescription: </td>
+        <td>
+    <input type=hidden name=Ds_Merchant_Titular value='<?=htmlentities($_data[Ds_Merchant_ProductDescription])?>'> <?=htmlentities($_data[Ds_Merchant_ProductDescription])?>
+    </td></tr>    <tr><td>Ds_Merchant_ProductDescription</td>
         <td>
     <input type=hidden name=Ds_Merchant_Amount value='<?=htmlentities($_data[Ds_Merchant_Amount])?>'> <?=htmlentities($_data[Ds_Merchant_Amount])?>
     </td></tr>
+
     <tr><td>Ds_Merchant_TransactionType: </td>
         <td>
     <input type=hidden name=Ds_Merchant_TransactionType value='<?=htmlentities($_data[Ds_Merchant_TransactionType])?>'> <?=htmlentities($_data[Ds_Merchant_TransactionType])?>
@@ -44,17 +70,30 @@
     </td></tr><tr><td>Ds_Merchant_Order: </td>
         <td>
     <input type=hidden name=Ds_Merchant_Order value='<?=htmlentities($_data[Ds_Merchant_Order])?>'> <?=htmlentities($_data[Ds_Merchant_Order])?>
-    </td></tr><tr><td>Ds_Merchant_MerchantCode: </td>
-        <td>
-    <input type=hidden name=Ds_Merchant_MerchantCode value='<?=htmlentities($_data[Ds_Merchant_MerchantCode])?>'> <?=htmlentities($_data[Ds_Merchant_MerchantCode])?>
-            
-    </td></tr><tr><td>Ds_Merchant_Terminal: </td>
+    </td></tr>
+
+    <tr><td>Ds_Merchant_Terminal: </td>
         <td> 
     <input type=hidden name=Ds_Merchant_Terminal value='<?=htmlentities($_data[Ds_Merchant_Terminal])?>'> <?=htmlentities($_data[Ds_Merchant_Terminal])?>
-    </td></tr><tr><td>Ds_Merchant_MerchantURL: </td>
+    </td></tr>
+    <tr><td>Ds_Merchant_MerchantURL: </td>
         <td>
     <input type=hidden name=Ds_Merchant_MerchantURL value='<?=htmlentities($_data[Ds_Merchant_MerchantURL])?>'> <?=htmlentities($_data[Ds_Merchant_MerchantURL])?>
-    </td></tr><tr><td>Ds_Merchant_MerchantSignature: </td>
+    </td></tr>
+    <tr><td>Ds_Merchant_UrlOK: </td>
+        <td>
+    <input type=hidden name=Ds_Merchant_UrlOK value='<?=htmlentities($_data[Ds_Merchant_UrlOK])?>'> <?=htmlentities($_data[Ds_Merchant_UrlOK])?>
+    </td></tr>
+    <tr><td>Ds_Merchant_UrlKO: </td>
+        <td>
+    <input type=hidden name=Ds_Merchant_UrlKO value='<?=htmlentities($_data[Ds_Merchant_UrlKO])?>'> <?=htmlentities($_data[Ds_Merchant_UrlKO])?>
+    </td></tr>
+    <tr><td>Ds_Merchant_MerchantData</td>
+        <td>
+    <input type=hidden name=Ds_Merchant_MerchantData value='<?=htmlentities($_data[Ds_Merchant_MerchantData])?>'> <?=htmlentities($_data[Ds_Merchant_MerchantData])?>
+    </td></tr>    
+    
+    <tr><td>Ds_Merchant_MerchantSignature: </td>
         <td>
     <input type=hidden name=Ds_Merchant_MerchantSignature value='<?=htmlentities($Ds_Merchant_MerchantSignature)?>'> <?=htmlentities($Ds_Merchant_MerchantSignature)?>
      [strtoupper(sha1(
@@ -67,6 +106,7 @@
                .$_data[secret]
                ));]
     </td></tr>
+    
   </table>         
    <input type=submit value='Go to Step 3/3'> it will target a _blank page.  
         
@@ -88,13 +128,38 @@
 <?php } ?>
         
         </td></tr>
+    <tr><td>Ds_Merchant_MerchantCode</td>
+        <td>
+<?php if(strlen($_data[Ds_Merchant_MerchantCode])) echo 'Provided'; else {?>
+    <input type=text name=Ds_Merchant_MerchantCode value='<?=(!strlen($_data[Ds_Merchant_MerchantCode]))?'327559290':htmlentities($_data[Ds_Merchant_MerchantCode])?>' (El código de tu comercio)>
+<?php } ?>
+    </td></tr>
+    <tr><td>Ds_Merchant_MerchantName</td>
+        <td>
+<?php if(strlen($_data[Ds_Merchant_MerchantName])) echo 'Provided'; else {?>
+    <input type=text name=Ds_Merchant_MerchantName value='<?=(!strlen($_data[Ds_Merchant_MerchantName]))?'Your Merchant Name':htmlentities($_data[Ds_Merchant_MerchantName])?>' (TPVSales Code)
+<?php } ?>
+    </td></tr>   
+    <tr><td>Ds_Merchant_Titular</td>
+        <td>
+<?php if(strlen($_data[Ds_Merchant_Titular])) echo 'Provided'; else {?>
+    <input type=text name=Ds_Merchant_Titular value='<?=(!strlen($_data[Ds_Merchant_Titular]))?'Transaction Title':htmlentities($_data[Ds_Merchant_Titular])?>' (it will be shown in the Page)
+<?php } ?>
+    </td></tr>   
+    <tr><td>Ds_Merchant_ProductDescription</td>
+        <td>
+<?php if(strlen($_data[Ds_Merchant_ProductDescription])) echo 'Provided'; else {?>
+    <input type=text name=Ds_Merchant_ProductDescription value='<?=(!strlen($_data[Ds_Merchant_ProductDescription]))?'Product to Buy':htmlentities($_data[Ds_Merchant_ProductDescription])?>' (it will be shown in the Page)
+<?php } ?>
+    </td></tr>                
     <tr><td>Ds_Merchant_Amount</td>
         <td>
  <?php if(strlen($_data[Ds_Merchant_Amount])) echo 'Provided'; else {?>
     <input type=text name=Ds_Merchant_Amount value='<?=(!strlen($_data[Ds_Merchant_Amount]))?'100':htmlentities($_data[Ds_Merchant_Amount])?>'> equivale a 1,00 (las dos ultimas posiciones son decimales)
 <?php } ?>
     </td></tr>
-    <tr><td>Ds_Merchant_TransactionType</td>
+
+     <tr><td>Ds_Merchant_TransactionType</td>
         <td>
 <?php if(strlen($_data[Ds_Merchant_TransactionType])) echo 'Provided'; else {?>
       else {?>
@@ -122,12 +187,9 @@
 <?php if(strlen($_data[Ds_Merchant_Order])) echo 'Provided'; else {?>
     <input type=text name=Ds_Merchant_Order  value='<?=(!strlen($_data[Ds_Merchant_Order]))?'0001PRUEBA':htmlentities($_data[Ds_Merchant_Order])?>'> (Max. 10 num/letras, siendo los 4 primeros números)
 <?php } ?>
-    </td></tr><tr><td>Ds_Merchant_MerchantCode</td>
-        <td>
-<?php if(strlen($_data[Ds_Merchant_MerchantCode])) echo 'Provided'; else {?>
-    <input type=text name=Ds_Merchant_MerchantCode value='<?=(!strlen($_data[Ds_Merchant_MerchantCode]))?'327559290':htmlentities($_data[Ds_Merchant_MerchantCode])?>' (El código de tu comercio)>
-<?php } ?>
-    </td></tr><tr><td>Ds_Merchant_Terminal</td>
+    </td></tr>
+
+    <tr><td>Ds_Merchant_Terminal</td>
         <td> 
 <?php if(strlen($_data[Ds_Merchant_Terminal])) echo 'Provided'; else {?>
             <select name='Ds_Merchant_Terminal'>
@@ -135,20 +197,38 @@
         <option  value=''>Para otros terminales solicitar al banco</option>
         </select>
 <?php } ?>
-    </td></tr><tr><td>Ds_Merchant_MerchantURL</td>
+    </td></tr>
+    
+    <tr><td>Ds_Merchant_MerchantURL</td>
         <td>
 <?php if(strlen($_data[Ds_Merchant_MerchantURL])) echo 'Provided'; else {?>
     <input type=text name=Ds_Merchant_MerchantURL value='<?=(!strlen($_data[Ds_Merchant_MerchantURL]))?'http://www.adnbp.com/CloudFrameWorkTPV/SABADELL/response':htmlentities($_data[Ds_Merchant_MerchantURL])?>'>
 <?php } ?>
-    
-    
-    </td></tr><tr><td>TPV_Secret</td>
+    </td></tr>
+    <tr><td>Ds_Merchant_UrlOK</td>
         <td>
-
+<?php if(strlen($_data[Ds_Merchant_UrlOK])) echo 'Provided'; else {?>
+    <input type=text name=Ds_Merchant_MerchantURL value='<?=(!strlen($_data[Ds_Merchant_UrlOK]))?'http://www.adnbp.com/CloudFrameWorkTPV/SABADELL/OK':htmlentities($_data[Ds_Merchant_UrlOK])?>'>
+<?php } ?>
+    </td></tr>  
+    <tr><td>Ds_Merchant_UrlKO</td>
+        <td>
+<?php if(strlen($_data[Ds_Merchant_UrlKO])) echo 'Provided'; else {?>
+    <input type=text name=Ds_Merchant_UrlKO value='<?=(!strlen($_data[Ds_Merchant_UrlKO]))?'http://www.adnbp.com/CloudFrameWorkTPV/SABADELL/KO':htmlentities($_data[Ds_Merchant_UrlKO])?>'>
+<?php } ?>
+    </td></tr>      
+    <tr><td>TPV_Secret</td>
+        <td>
 <?php if(strlen($_data[TPV_Secret])) echo 'Provided'; else {?>
     <input type=password name=secret  value='<?=(!strlen($_data[TPV_Secret]))?'':htmlentities($_data[TPV_Secret])?>'> (are different in dev & prod )
 <?php } ?>
    
+    </td></tr>
+    <tr><td>Ds_Merchant_MerchantData</td>
+        <td>
+ <?php if(strlen($_data[Ds_Merchant_MerchantData])) echo 'Provided'; else {?>
+    <input type=text name=Ds_Merchant_MerchantData value='<?=(!strlen($_data[Ds_Merchant_MerchantData]))?'':htmlentities($_data[Ds_Merchant_MerchantData])?>'> Libre Texto devuelto por el TPV
+<?php } ?>
     </td></tr>
   </table>
   <input type=submit value='Go to Step 2/3'>  
