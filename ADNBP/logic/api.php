cc356f47d9b93aae236a480f7238379b37d54197
ADRIAN MARTINEZ <adrianmartinez@ADRIANs-iMac.local>
2015-03-30 13:57:40 +0200

    add session id in the debug

--- ADNBP/logic/api.php
+++ ADNBP/logic/api.php
@@ -51,6 +51,7 @@
 		if(isset($api->formParams['debug'])) {
 			$ret['header'] = $api->getHeader();
 	        $ret['method'] = $api->method;
+			$ret['session'] = session_id();
 			$ret['ip']=$this->_ip;
 	        $ret['url']=(($_SERVER['HTTPS']=='on')?'https://':'http://').$_SERVER['HTTP_HOST'].'/'.$_SERVER['REQUEST_URI'];
 	        $ret['user_agent']=($this->userAgent!=null)?$this->userAgent:$api->requestHeaders['User-Agent'];
