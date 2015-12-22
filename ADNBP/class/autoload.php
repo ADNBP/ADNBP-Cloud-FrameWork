79b5154d581a319ed5b14196e7cb638bd664d65f
Fran López <fl@bloombees.com>
2015-12-22 10:57:44 +0100

    Refactoring Cloud Framework

---
+++ ADNBP/class/autoload.php
@@ -0,0 +1,15 @@
+<?php
+    /**
+     * General autoloader
+     */
+
+    if (file_exists(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php')) {
+        require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+    }
+
+    //Social network autoloader
+    require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'socialnetworks' . DIRECTORY_SEPARATOR . 'autoload.php';
+    //Notifications autoloader
+    require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'notifications' . DIRECTORY_SEPARATOR . 'autoload.php';
+    //Datastore autoloader
+    require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'datastore' . DIRECTORY_SEPARATOR . 'autoload.php';
