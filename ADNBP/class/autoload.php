13d7c738650d0e7159fe55d071b05dfc265b09e3
Salvador Castro <sc@bloombees.com>
2016-03-11 09:41:52 +0100

    Connector improvements

--- ADNBP/class/autoload.php
+++ ADNBP/class/autoload.php
@@ -7,10 +7,11 @@
         require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
     }
     //CloudFramework
-    require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'cloudframework-io' . DIRECTORY_SEPARATOR . 'autoload.php';
+    //require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'cloudframework-io' . DIRECTORY_SEPARATOR . 'autoload.php';
 
     //Social network autoloader
-    require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'socialnetworks' . DIRECTORY_SEPARATOR . 'autoload.php';
+    //require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'socialnetworks' . DIRECTORY_SEPARATOR . 'autoload.php';
+
     //Notifications autoloader
     require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'notifications' . DIRECTORY_SEPARATOR . 'autoload.php';
     //Datastore autoloader
