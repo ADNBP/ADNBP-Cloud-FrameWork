5a3011e13c84198bf18e0233a0d782cf5c2d0a7a
Adrian Martinez <am@bloombees.com>
2016-04-11 14:20:15 +0200

    Updating API structure

---
+++ cloudframework_appengine_php/src/api/h/_test.php
@@ -0,0 +1,65 @@
+<?php
+class API extends RESTful
+{
+	function main()
+	{
+		$this->checkMethod('GET');
+
+		if(!isset($_GET['only']) || $_GET['only']=='db')
+		if ($this->core->config->get("dbName")) {
+			$this->core->__p->init('test', 'CloudSQL connect');
+			$db = $this->core->loadClass("CloudSQL");
+			$db->connect();
+			if (!$db->error()) $db->close();
+			$notes = array($db->getError());
+			/*
+			$notes[] = ['dbServer'=>(strlen($this->core->config->get("dbServer")))?substr($this->core->config->get("dbServer"),0,4).'***':'None'];
+			$notes[] = ['dbSocket'=>(strlen($this->core->config->get("dbSocket")))?'***':'None'];
+			$notes[] = ['dbUser'=>(strlen($this->core->config->get("dbUser")))?'***':'None'];
+			$notes[] = ['dbPassword'=>(strlen($this->core->config->get("dbPassword")))?'***':'None'];
+			$notes[] = ['dbName'=>(strlen($this->core->config->get("dbName")))?'***':'None'];
+			$notes[] = ['dbPort'=>(strlen($this->core->config->get("dbPort")))?'***':'None'];
+			*/
+			$this->core->__p->end('test', 'CloudSQL connect', !$db->error(), $notes);
+		} else {
+			$this->addReturnData(array('CloudSQL connect' => 'no DB configured'));
+		}
+
+		if(!isset($_GET['only']) || $_GET['only']=='localize')
+		if (strlen($this->core->config->get("LocalizePath"))) {
+			$this->core->__p->init('test', 'LocalizePath scandir');
+			$errMsg = '';
+			try {
+				$ret = scandir($this->core->config->get("LocalizePath"));
+			} catch (Exception $e) {
+				$errMsg = 'Error reading ' . $this->core->config->get("LocalizePath") . ': ' . $e->getMessage() . ' ' . error_get_last();
+			}
+			$this->core->__p->end('test', 'LocalizePath scandir', is_array($ret), $this->core->config->get("LocalizePath") . ': ' . $errMsg);
+		}
+
+		if(!isset($_GET['only']) || $_GET['only']=='cloud') {
+			/* @var CloudServiceRequest $sc */
+			$cs = $this->core->loadClass('CloudServiceRequest');
+			if ($cs->getServiceUrl()) {
+				$this->core->__p->init('test', 'Cloud Service Url');
+				$ret = $cs->get('/_version');
+				if (!$cs->error) {
+					$ret = json_decode($ret);
+					$retOk = $ret->success;
+					if (!$retOk) $retErr = json_encode($ret);
+				} else {
+					$retOk = false;
+				}
+				$this->core->__p->end('test', 'Cloud Service Url', $retOk, $cs->getServiceUrl('/_version') . ' ' . $retErr);
+			} else {
+				$this->addReturnData(array('Cloud Service Url' => 'no CloudServiceUrl configured'));
+			}
+		}
+
+		if (is_file($this->core->system->app_path . '/logic/_test.php')) include($this->core->system->app_path  . '/logic/_test.php');
+
+		if (isset($this->core->__p->data['init']))
+			$this->addReturnData($this->core->__p->data['init']['test']);
+	}
+}
+
---
+++ cloudframework_appengine_php/tests/Core.php
@@ -0,0 +1,5 @@
+<?php
+include_once __DIR__.'/../src/Core.php';
+$core = new Core();
+
+_print($core->__p->data['info']);
