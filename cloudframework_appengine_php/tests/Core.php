069a8848cb062edbd2038a4efdc936c661c008a9
am-adnbp <ApoloZeus99>
2016-04-20 06:46:02 +0200

    Moving appengine-php-core

--- cloudframework_appengine_php/tests/Core.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once __DIR__.'/../src/Core.php';
-$core = new Core();
-
-// Check CloudService
-$ret = $core->request->get('/_version');
-$ret = $core->request->getCurl('/_version');
-
-_print($core->__p->data);
