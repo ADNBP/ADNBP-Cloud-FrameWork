c13a884755aa7608e7dfb88738b7b630d5e2f56c
Fran López <fl@bloombees.com>
2016-05-03 20:30:25 +0200

    Adding templates functionality to ADNBP

--- ADNBP/adnbppl.php
+++ ADNBP/adnbppl.php
@@ -1,8 +1,8 @@
 <?php
 // Copy this file in the upper folder of ADNBP framework folder
 // v14 Aug. 2015
-include_once ("ADNBP/__performance.php"); // it include __p(),_print(),_printe() funtions to trace and performance specific points
-require_once ("ADNBP/class/ADNBP.php");
-$adnbp = new ADNBP();
-$adnbp -> run();
-?>
+    include_once("ADNBP/__performance.php"); // it include __p(),_print(),_printe() funtions to trace and performance specific points
+    require_once("ADNBP/class/ADNBP.php");
+    $adnbp = new ADNBP();
+    $adnbp->run();
+    
